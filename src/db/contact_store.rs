d72c5197c4a2a4b2bbfee6663df39e2c93af4542
Arseni Laputska <arsenilaputska@gmail.com>
2025-02-19 17:52:55 +0300

    fixed objc_contact build errors

--- src/db/contact_store.rs
+++ src/db/contact_store.rs
@@ -1,18 +1,39 @@
 // src/db/contacts_store.rs
 
 use objc2::declare::ClassDecl;
-// Не используем импорт из objc2::runtime::ClassType – вместо этого для вызова методов класса используем методы из objc2_foundation.
-// Импорт макроса sel! для построения селекторов:
-use objc2::sel;
-use objc2_foundation::{NSObject, NSArray, NSMutableArray, NSString};
-use objc2::runtime::{Sel, Ivar, Class, Object};
-use objc2::msg_send;
+use objc2_foundation::{NSObject, NSArray, NSMutableArray};
+use objc2::runtime::{Sel, AnyClass, AnyObject, Object};
+use objc2::{msg_send, sel, Encode, Encoding, RefEncode, Message};
 use std::ptr;
 use std::sync::Once;
 use std::ffi::{CString, CStr};
-use once_cell::sync::Lazy;
-use crate::db::objc_contact::{RustContact, register_rust_contact_class};
-use crate::db::objc_converters::{convert_to_nsdata, convert_to_nsstring};
+use crate::db::objc_contact::{RustContact};
+
+extern "C" {
+    fn object_getInstanceVariable(
+        obj: *mut Object,
+        name: *const i8,
+        out_val: *mut *mut std::os::raw::c_void
+    ) -> *mut std::os::raw::c_void;
+
+    fn object_setInstanceVariable(
+        obj: *mut Object,
+        name: *const i8,
+        value: *mut std::os::raw::c_void
+    ) -> *mut std::os::raw::c_void;
+}
+
+unsafe fn get_ivar_raw<T>(obj: *mut Object, ivar_name: &str) -> *mut T {
+    let c_name = CString::new(ivar_name).unwrap();
+    let mut out_val: *mut std::os::raw::c_void = std::ptr::null_mut();
+    object_getInstanceVariable(obj, c_name.as_ptr(), &mut out_val);
+    out_val as *mut T
+}
+
+unsafe fn set_ivar_raw<T>(obj: *mut Object, ivar_name: &str, value: *mut T) {
+    let c_name = CString::new(ivar_name).unwrap();
+    let _old_val = object_setInstanceVariable(obj, c_name.as_ptr(), value as *mut _);
+}
 
 // Регистрация класса ContactsStore (наследника NSObject), который хранит массив контактов.
 static CONTACTS_STORE_REGISTER: Once = Once::new();
@@ -20,26 +41,27 @@ static mut CONTACTS_STORE_CLASS: *const objc2::runtime::Class = ptr::null();
 
 /// Регистрирует класс "ContactsStore" с одним ivar‑ом "_contacts" (NSMutableArray)
 /// и добавляет геттер и сеттер для свойства "contacts" с KVO‑уведомлениями.
-pub fn register_contacts_store_class() -> &'static Class {
+pub fn register_contacts_store_class() -> &'static AnyClass {
     CONTACTS_STORE_REGISTER.call_once(|| {
-        // Получаем класс NSObject через runtime: передаем имя как &CStr
-        let nsobject_class = Class::get(CStr::from_bytes_with_nul(b"NSObject\0").unwrap())
+        let nsobject_class = AnyClass::get(CStr::from_bytes_with_nul(b"NSObject\0").unwrap())
             .expect("NSObject class not found");
         let class_name = CStr::from_bytes_with_nul(b"ContactsStore\0").unwrap();
+
         let mut decl = ClassDecl::new(class_name, nsobject_class)
             .expect("Failed to declare ContactsStore class");
 
-        // Добавляем ivar "_contacts". Имя ivar передаем как &CStr.
+        // Добавляем ivar "_contacts"
         decl.add_ivar::<*mut NSMutableArray>(CStr::from_bytes_with_nul(b"_contacts\0").unwrap());
 
         unsafe {
+            // Методы объявляем как extern "C" fn(*mut ContactsStore, Sel, ...)
             decl.add_method(
                 sel!(contacts),
-                contacts_getter as extern "C" fn(&ContactsStore, _cmd: _ ) -> *mut NSArray,
+                contacts_getter as extern "C" fn(*mut ContactsStore, Sel) -> *mut NSArray,
             );
             decl.add_method(
                 sel!(setContacts:),
-                contacts_setter as extern "C" fn(&mut ContactsStore, _cmd: _, *mut NSArray),
+                contacts_setter as extern "C" fn(*mut ContactsStore, Sel, *mut NSArray),
             );
         }
 
@@ -59,27 +81,47 @@ pub struct ContactsStore {
 
 // Для того чтобы ContactsStore можно было использовать в FFI,
 // возможно потребуется реализовать Encode. Например, вручную:
-unsafe impl objc2::Encode for ContactsStore {
-    const ENCODING: &'static str = "{ContactsStore=#}";
+// Исправляем ENCODING и реализуем необходимые трейты.
+unsafe impl Encode for ContactsStore {
+    // Вместо &str указываем Encoding
+    const ENCODING: Encoding = Encoding::Struct("{ContactsStore=}", &[]);
 }
+unsafe impl RefEncode for ContactsStore {
+    const ENCODING_REF: Encoding = Encoding::Pointer(&Self::ENCODING);
+}
+unsafe impl Message for ContactsStore {}
 
 /// Геттер для свойства "contacts"
-extern "C" fn contacts_getter(this: &ContactsStore, _cmd: Sel) -> *mut NSArray {
+extern "C" fn contacts_getter(this: *mut ContactsStore, _cmd: Sel) -> *mut NSArray {
     unsafe {
-        // Получаем значение ivar "_contacts"
-        this.get_ivar::<*mut NSMutableArray>("_contacts")
-            .map(|p| p as *mut NSArray)
-            .unwrap_or(ptr::null_mut())
+        let obj_ptr = &mut (*this).superclass as *mut NSObject as *mut Object;
+
+        // Читаем ivar "_contacts"
+        let arr_ptr = get_ivar_raw::<NSMutableArray>(obj_ptr, "_contacts");
+        if arr_ptr.is_null() {
+            ptr::null_mut()
+        } else {
+            arr_ptr as *mut NSArray
+        }
     }
 }
 
 /// Сеттер для свойства "contacts" с обёрткой KVO (will/didChangeValueForKey:)
-extern "C" fn contacts_setter(this: &mut ContactsStore, _cmd: Sel, new_contacts: *mut NSArray) {
+
+extern "C" fn contacts_setter(this: *mut ContactsStore, _cmd: Sel, new_contacts: *mut NSArray) {
     unsafe {
         let key = CString::new("contacts").unwrap();
-        let _: () = msg_send![this, willChangeValueForKey:key.as_ptr()];
-        this.set_ivar("_contacts", new_contacts as *mut NSMutableArray);
-        let _: () = msg_send![this, didChangeValueForKey:key.as_ptr()];
+        let obj_ptr = &mut (*this).superclass as *mut NSObject as *mut Object;
+
+        // willChangeValueForKey:
+        let _: () = msg_send![obj_ptr, willChangeValueForKey: key.as_ptr()];
+
+        // Записываем в ivar "_contacts"
+        let new_mmarr = new_contacts as *mut NSMutableArray;
+        set_ivar_raw(obj_ptr, "_contacts", new_mmarr);
+
+        // didChangeValueForKey:
+        let _: () = msg_send![obj_ptr, didChangeValueForKey: key.as_ptr()];
     }
 }
 
@@ -88,13 +130,17 @@ pub fn new_contacts_store() -> *mut ContactsStore {
     let cls = register_contacts_store_class();
     unsafe {
         let store: *mut ContactsStore = msg_send![cls, new];
-        // Получаем класс NSMutableArray через runtime
-        let nsmutablearray_class = Class::get(CStr::from_bytes_with_nul(b"NSMutableArray\0").unwrap())
+
+        // Создаём пустой NSMutableArray
+        let nsma_class = AnyClass::get(CStr::from_bytes_with_nul(b"NSMutableArray\0").unwrap())
             .expect("NSMutableArray class not found");
-        let empty_array: *mut NSMutableArray = msg_send![nsmutablearray_class, alloc];
-        let empty_array: *mut NSMutableArray = msg_send![empty_array, init];
-        (&mut (*store).superclass)
-            .set_ivar(CStr::from_bytes_with_nul(b"_contacts\0").unwrap(), empty_array);
+        let empty_arr: *mut NSMutableArray = msg_send![nsma_class, alloc];
+        let empty_arr: *mut NSMutableArray = msg_send![empty_arr, init];
+
+        // Пишем в ivar
+        let obj_ptr = &mut (*store).superclass as *mut NSObject as *mut Object;
+        set_ivar_raw(obj_ptr, "_contacts", empty_arr);
+
         store
     }
 }
@@ -103,15 +149,19 @@ pub fn new_contacts_store() -> *mut ContactsStore {
 /// Принимается вектор указателей на объекты RustContact (которые являются нашими представлениями контактов).
 pub fn update_contacts(store: *mut ContactsStore, contacts: Vec<*mut RustContact>) {
     unsafe {
-        let nsmutablearray_class = Class::get(CStr::from_bytes_with_nul(b"NSMutableArray\0").unwrap())
+        let nsma_class = AnyClass::get(CStr::from_bytes_with_nul(b"NSMutableArray\0").unwrap())
             .expect("NSMutableArray class not found");
-        let ns_mut_array: *mut NSMutableArray = msg_send![nsmutablearray_class, alloc];
-        let ns_mut_array: *mut NSMutableArray = msg_send![ns_mut_array, init];
-        for contact in contacts {
-            // Приводим RustContact к NSObject (первое поле – superclass)
-            let obj: *mut NSObject = contact as *mut NSObject;
-            let _: () = msg_send![ns_mut_array, addObject: obj];
+
+        let arr: *mut NSMutableArray = msg_send![nsma_class, alloc];
+        let arr: *mut NSMutableArray = msg_send![arr, init];
+
+        // Добавляем объекты
+        for c in contacts {
+            // RustContact -> superclass -> *mut NSObject
+            let c_obj = c as *mut NSObject;
+            let _: () = msg_send![arr, addObject: c_obj];
         }
-        let _: () = msg_send![store, setContacts: ns_mut_array];
+        // Вызываем сеттер (setContacts:) => KVO
+        let _: () = msg_send![store, setContacts: arr];
     }
 }
